5e24fffd8690f33161ad8ce74bbb02ea3a71e892
gaucho-v <53967056+gaucho-v@users.noreply.github.com>
2020-04-22 19:14:18 +0300

    Updates

---
+++ precache-manifest.ca9abfb028194768fa0dfc4329690d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14630e9a8bcf01a4844762b02cd9765c",
+    "url": "/covid2019-visual/index.html"
+  },
+  {
+    "revision": "1a5c13d6ceb922b51659",
+    "url": "/covid2019-visual/static/js/2.d4c9d9be.chunk.js"
+  },
+  {
+    "revision": "8055f3f4ee31f8e7cc96eaef3ebd9569",
+    "url": "/covid2019-visual/static/js/2.d4c9d9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c19dcc0c42d4a0fe81f",
+    "url": "/covid2019-visual/static/js/main.4c2cf792.chunk.js"
+  },
+  {
+    "revision": "169f9c1f615e6fb9b940",
+    "url": "/covid2019-visual/static/js/runtime-main.f8f6f12c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid2019-visual/precache-manifest.91d6748d3279f12aafa746f2bb1955f5.js"
+  "/covid2019-visual/precache-manifest.ca9abfb028194768fa0dfc4329690d45.js"
 );
 
 self.addEventListener('message', (event) => {
