64194bc8621a01fd6b4a4eaeb016db358805e0ca
gaucho-v <53967056+gaucho-v@users.noreply.github.com>
2020-05-03 16:13:26 +0300

    Updates

---
+++ precache-manifest.f8c2d05665479451a74a8be3c94f28c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b8e1cc9e1cfaaada82e11a7074af30",
+    "url": "/covid2019-visual/index.html"
+  },
+  {
+    "revision": "8faf5b6969cf286ecdc5",
+    "url": "/covid2019-visual/static/css/main.e0be0486.chunk.css"
+  },
+  {
+    "revision": "f6bb361d4c203419c9fe",
+    "url": "/covid2019-visual/static/js/2.cda85103.chunk.js"
+  },
+  {
+    "revision": "8055f3f4ee31f8e7cc96eaef3ebd9569",
+    "url": "/covid2019-visual/static/js/2.cda85103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8faf5b6969cf286ecdc5",
+    "url": "/covid2019-visual/static/js/main.338ac578.chunk.js"
+  },
+  {
+    "revision": "169f9c1f615e6fb9b940",
+    "url": "/covid2019-visual/static/js/runtime-main.f8f6f12c.js"
+  }
+]);
